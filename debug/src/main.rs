318100fec46cb249c6b2e77c3e78ae00a8edec5c
block0xhash <crypt0mate@proton.me>
2024-04-07 01:33:41 +0200

    Added fmt::Display trait implementation on vector

--- debug/src/main.rs
+++ debug/src/main.rs
@@ -65,6 +65,30 @@ impl fmt::Display for Point2D {
     }
 }
 
+// Define a structure named `List` containing a `Vec`.
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Extract the value using tuple indexing,
+        // and create a reference to `vec`.
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        // Iterate over `v` in `vec` while enumerating the iteration
+        // count in `count`.
+        for (count, v) in vec.iter().enumerate() {
+            // For every element except the first, add a comma.
+            // Use the ? operator to return on errors.
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{count}: {v}")?;
+        }
+
+        // Close the opened bracket and return a fmt::Result value.
+        write!(f, "]")
+    }
+}
 
 
 fn main() {
@@ -111,7 +135,9 @@ fn main() {
     println!("Display: {}", point);
     println!("Debug: {:?}", point);
 
-
+    //use Display on a vector
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
 
 
 
